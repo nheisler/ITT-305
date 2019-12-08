f69ac2f85c8dd2cecd6c792227ea50cfdded8fff
nheisler <42762547+nheisler@users.noreply.github.com>
2019-12-07 20:25:35 -0700

    Update search_keyword.php

--- search_keyword.php
+++ search_keyword.php
@@ -26,8 +26,7 @@ $keywordfromform = $_GET["keyword"];
 $keywordfromform = "%" . $keywordfromform . "%";
 
 //Search the database for the word chicken
-//echo"<h2>Show all jokes with the word $keywordfromform</h2>";
-echo"<h2>SQL" . $mysqli->error . "</h2>";
+echo"<h2>Show all jokes with the word $keywordfromform</h2>";
 
 $stmt = $mysqli->prepare("SELECT JokeID, Joke_question, Joke_answer, users_id, username FROM Jokes_table JOIN users ON users.id = jokes_table.users_id WHERE Joke_question LIKE ?");
 
@@ -54,6 +53,8 @@ if ($stmt->num_rows > 0) {
 } else {
     echo "0 results";
 }
+
+echo"<h2>SQL" . $mysqli->info . "</h2>";
 
 ?>
 
